094a7783af527a1b4ba17d4db3ab6720e440eda5
redhookutsav <87003220+redhookutsav@users.noreply.github.com>
2026-08-23 22:01:55 +0530

    Update Package.swift liquid glass

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "Traffy",
     platforms: [
-        .macOS(.v13)
+        .macOS(.v26)
     ],
     targets: [
         .executableTarget(
